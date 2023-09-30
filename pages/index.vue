b1c961e07c85ebe2658d02428fd48942363356ee
Bruno Lopes <br.lopes22@gmail.com>
2023-09-30 06:58:36 -0400

    commit with modifications in the images and the about section

--- pages/index.vue
+++ pages/index.vue
@@ -1,25 +1,17 @@
 <template>
-
-<div class="container text-center">
-  <div class="row align-items-start">
-    <div class="col-3">
-      <h1>Hello 1, {{ message }}</h1>
-    </div>
-    <div class="col-5">
-      <h1>trying boostrap</h1>
-    </div>
-    <div class="col-4">
-      working?
-    </div>
-  </div>
-</div>
+  <about/>
 
 </template>
 
  
 
 <script>
+import about from '../components/about.vue';
+
 export default {
+  components: {
+    about
+  },
   data() {
     return {
       message: 'Vue.js'
