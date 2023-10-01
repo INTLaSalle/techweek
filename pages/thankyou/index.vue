f28ad908b962e6cf435bd2e18f5c171243cdb057
Thiago Soares de Souza <thiago.souza.jobs@gmail.com>
2023-10-01 19:45:31 -0400

    feat: create a thankyou page that will be used after the user send a message in the contact form from the contact page

---
+++ pages/thankyou/index.vue
@@ -0,0 +1,31 @@
+<template>
+    <client-only>
+        <thankyou/>
+    </client-only>
+  
+  </template>
+  
+   
+  
+  <script>
+  import thankyou from '../components/thankyou.vue';
+  
+  export default {
+    components: {
+      thankyou
+    },
+    data() {
+      return {
+        message: 'Vue.js'
+      };
+    }
+  }
+  </script>
+  
+   
+  
+  <style scoped>
+  h1 {
+    color: blue;
+  }
+  </style>
