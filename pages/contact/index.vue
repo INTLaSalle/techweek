ee9e3a23cf6c1fa5e399e94602dac78073b1f9e5
Laura Silva <72063248+LauraSilva1401@users.noreply.github.com>
2023-10-08 15:58:22 -0400

    Revert "Revert 11 main"

---
+++ pages/contact/index.vue
@@ -0,0 +1,32 @@
+<template>
+    <join/>
+    <contact/>
+  
+  </template>
+  
+   
+  
+  <script>
+  import join from '../components/join.vue';
+  import contact from '../components/contact.vue';
+  
+  export default {
+    components: {
+      join,
+      contact
+    },
+    data() {
+      return {
+        message: 'Vue.js'
+      };
+    }
+  }
+  </script>
+  
+   
+  
+  <style scoped>
+  h1 {
+    color: blue;
+  }
+  </style>
