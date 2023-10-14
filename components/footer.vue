ad27559a4406e367251c816ea2bab34fed0c519c
Bruno Lopes <br.lopes22@gmail.com>
2023-10-14 09:30:00 -0400

    commit with changes in the file footer

---
+++ components/footer.vue
@@ -0,0 +1,32 @@
+
+<template>
+    <section id="footer">
+        <footer class="text-center text-lg-start" style="background-color: #db6930;">
+            <div class="footerImgs">
+                <a href="https://www.facebook.com/profile.php?id=100087291936912" target="_blank">
+                    <img id="face" alt="Facebook" src="../images/icons8-facebook-white.svg"
+                        style="width: 25px; height: 25px; object-fit: cover;">
+                </a>
+                <a href="https://www.instagram.com/int.lasalle/" target="_blank">
+                    <img id="inst" alt="Instagram" src="../images/icons8-instagram-white.svg"
+                        style="width: 25px; height: 25px; object-fit: cover;">
+                </a>
+            </div>
+
+            <!-- Copyright -->
+            <div class="text-center text-white p-3" style="background-color: rgba(0, 0, 0, 0.2);">
+                © 2020 Copyright:
+                <a class="text-white" href="https://mdbootstrap.com/">MDBootstrap.com</a>
+            </div>
+            <!-- Copyright -->
+        </footer>
+
+    </section>
+</template>
+
+<script>
+
+</script>
+
+<style scoped></style>
+    
--- components/navbar.vue
+++ components/navbar.vue
@@ -1,57 +1,56 @@
 <template>
 	<nav class="navbar navbar-expand-lg bg-body-tertiary fixed-top">
-	  <div class="container-fluid">
-	    
-		<a class="navbar-brand" href="#">
-      		
-			<img src="../assets/img/INTLogo.png" class="img-fluid IntLogo" alt="INT LOGO">
-    	</a>
-	    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-	      <span class="navbar-toggler-icon"></span>
-	    </button>
-	    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-	      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+		<div class="container-fluid">
 
-	        <li class="nav-item">
-	          <a class="nav-link active" aria-current="page" href="/">HOME</a>
-	        </li>
+			<a class="navbar-brand" href="#">
 
-	        <li class="nav-item">
-	          <a class="nav-link" href="/#carouselExample">ABOUT</a>
-	        </li>
+				<img src="../assets/img/INTLogo.png" class="img-fluid IntLogo" alt="INT LOGO">
+			</a>
+			<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+				aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+				<span class="navbar-toggler-icon"></span>
+			</button>
+			<div class="collapse navbar-collapse" id="navbarSupportedContent">
+				<ul class="navbar-nav me-auto mb-2 mb-lg-0">
 
-			<li class="nav-item">
-	          <a class="nav-link" href="/#speakers">SPEAKERS</a>
-	        </li>
+					<li class="nav-item">
+						<a class="nav-link active" aria-current="page" href="/">HOME</a>
+					</li>
 
-			<li class="nav-item">
-	          <a class="nav-link" href="/#Schedule">SCHEDULE</a>
-	        </li>
+					<li class="nav-item">
+						<a class="nav-link" href="/#carouselExample">ABOUT</a>
+					</li>
 
-			<!-- <li class="nav-item">
+					<li class="nav-item">
+						<a class="nav-link" href="/#speakers">SPEAKERS</a>
+					</li>
+
+					<li class="nav-item">
+						<a class="nav-link" href="/#Schedule">SCHEDULE</a>
+					</li>
+
+					<!-- <li class="nav-item">
 	          <a class="nav-link" href="#">VENUE</a>
 	        </li> -->
 
-			<li class="nav-item">
-	          <a class="nav-link" href="/contact">JOIN / CONTACT INT</a>
-	        </li>
+					<li class="nav-item">
+						<a class="nav-link" href="/contact">JOIN / CONTACT INT</a>
+					</li>
 
-			<li class="nav-item">
-	          <a class="nav-link" href="#">DOWNLOAD APP</a>
-	        </li>
+					<li class="nav-item">
+						<a class="nav-link" href="#">DOWNLOAD APP</a>
+					</li>
 
 
-	      </ul>
-	      
-	    </div>
-	  </div>
+				</ul>
+
+			</div>
+		</div>
 	</nav>
 </template>
 
 <script>
-	
+
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
