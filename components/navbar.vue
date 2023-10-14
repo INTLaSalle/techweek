9f8ed4d47893bc29de0f63a7e7dfbc454fa42b55
Laura Silva <laura.c.silva01@gmail.com>
2023-10-14 13:45:25 -0400

    Fixed Navbar image path

--- components/navbar.vue
+++ components/navbar.vue
@@ -4,7 +4,7 @@
 	    
 		<a class="navbar-brand" href="#">
       		
-			<img :src="`../images/INTLogo.png" class="img-fluid IntLogo`" alt="INT LOGO">
+			<img :src="`../images/INTLogo.png`" class="img-fluid IntLogo" alt="INT LOGO">
     	</a>
 	    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
 	      <span class="navbar-toggler-icon"></span>
